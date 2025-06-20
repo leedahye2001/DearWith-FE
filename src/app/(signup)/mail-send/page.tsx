7345a37df8e675060c00160f5d4c8e6a45389651
leedahye <leedahye2001@naver.com>
2025-06-20 15:27:29 +0900

    hotfix: onClick 기능 추가

--- src/app/(signup)/mail-send/page.tsx
+++ src/app/(signup)/mail-send/page.tsx
@@ -1,62 +1,50 @@
 "use client";
 
-// import Button from "@/components/Button/Button";
-import Checkbox from "@/components/Checkbox/Checkbox";
 import { useState } from "react";
-// import { getMailSend } from "@/apis/api";
-// import Input from "@/components/Input/Input";
-// import { useState } from "react";
-// import { useRouter } from "next/navigation";
+import { getMailSend } from "@/apis/api";
+import Input from "@/components/Input/Input";
+import Button from "@/components/Button/Button";
+import { useRouter } from "next/navigation";
 
 const Page = () => {
-  // const router = useRouter();
+  const router = useRouter();
 
-  // const fetchMailData = async () => {
-  //   try {
-  //     // await getMailSend(email);
-  //     alert(
-  //       "이메일로 코드가 전송되었습니다. 전송된 인증 코드를 아래에 입력해주세요."
-  //     );
-  //     router.push("/mail-verify");
-  //   } catch (error) {
-  //     console.error("Error fetching data:", error);
-  //   }
-  // };
-  // const [text, setText] = useState<string>("");
-
-  // const handleTextChange = (text: string) => {
-  //   setText(text);
-  // };
-  // const [check, setCheck] = useState<boolean>();
-  // const handleCheckedChange = () => {
-  //   setCheck(check);
-  // };
-  const [isChecked, setIsChecked] = useState(true);
-  const handleCheckedChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const checked = e.target.checked;
-    setIsChecked(checked);
+  const fetchMailData = async () => {
+    try {
+      await getMailSend(email);
+      alert(
+        "이메일로 코드가 전송되었습니다. 전송된 인증 코드를 아래에 입력해주세요."
+      );
+      router.push("/mail-verify");
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
   };
+  const [email, setEmail] = useState<string>("");
+
+  const handleEmailSendChange = (email: string) => {
+    setEmail(email);
+  };
+
   return (
     <div className="bg-bg-1 dark:bg-bg-1 flex flex-col justify-center">
-      {/* <h1>디어위드에서 사용할 이메일을 입력해 주세요</h1> */}
-      {/* <Input
-        _value={text}
+      <h1 className="font-[700] text-text-5 text-[20px] mb-[40px]">
+        디어위드에서 사용할 <br />
+        이메일을 입력해 주세요
+      </h1>
+      <Input
+        _value={email}
         _state="textbox-underline"
-        _title="제목"
+        _title="이메일"
+        _bottomNode="메일 형식이 올바르지 않습니다"
         _view
-        _onChange={handleTextChange}
-      /> */}
-      {/* <button className="hover:cursor-pointer" onClick={fetchMailData}>
-        이메일 인증 요청하기
-      </button> */}
-      {/* <Button _state="sub" _node="버튼" /> */}
-      <Checkbox
-        _id="checkbox"
-        _value="체크"
-        _checked={isChecked}
-        _type="neutral1100"
-        _onChange={handleCheckedChange}
-        _inputWrapperProps={{ className: "text-text-5" }}
+        _onChange={handleEmailSendChange}
+      />
+      <Button
+        _state="main"
+        _node="이메일 인증 요청하기"
+        _buttonProps={{ className: "hover:cursor-pointer" }}
+        _onClick={fetchMailData}
       />
     </div>
   );
