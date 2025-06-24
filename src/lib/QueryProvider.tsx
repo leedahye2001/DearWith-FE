53904d94f60e0cf3d7be324c2715cdd27baaabe4
leedahye <leedahye2001@naver.com>
2025-06-24 17:17:56 +0900

    chore: remove 'recoil'

--- src/lib/QueryProvider.tsx
+++ src/lib/QueryProvider.tsx
@@ -3,14 +3,11 @@
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useState } from "react";
-import { RecoilRoot } from "recoil";
 
 export function QueryProvider({ children }: { children: React.ReactNode }) {
   const [queryClient] = useState(() => new QueryClient());
 
   return (
-    <RecoilRoot>
-      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-    </RecoilRoot>
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   );
 }
